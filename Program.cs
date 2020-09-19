93fb42da6f5b8c9f776275f953144af283abc722
KellyCristinaAires <k.woys@hotmail.com>
2020-09-19 16:25:13 -0300

    atividade cebolinha

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase;
+            Console.Write("Digite Qualquer Coisa: ");
+            frase = Console.ReadLine();
+            Console.Write("\n");
+
+            Console.WriteLine($"{frase.Replace("r", "l").Replace("R", "L")}");
+            Console.WriteLine("Pressione ENTER para sair");
+            Console.ReadKey();
+        }
+    }
+}
+
